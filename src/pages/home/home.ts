01bb38ed5c447b50e39bbe1ae26308f1f473ff7e
joaortizro <joaortizro@unal.edu.co>
2018-01-04 16:26:42 -0500

    Steven feedback

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,40 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {UserPage} from '../user/user'
-import {UsersProvider} from '../../providers/users/users'
-
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { UserPage } from "../user/user";
+import { UsersProvider } from "../../providers/users/users";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  public users: any;
+  public info: any;
 
-  public users:any;
-  public info:any;
-
-
-  constructor(public navCtrl: NavController , public usersProvider : UsersProvider) {
-
-  }
-
-
-
-  loadUser(){
-    return this.usersProvider.getUsers().subscribe( (response)=>{
-      this.users=response.results;
-      this.info=response.info;
-      console.log(this.users);
-      console.log(this.info);
-    }
-    )
-  }
-
-  ngOnInit(){
+  constructor(
+    public navCtrl: NavController,
+    public usersProvider: UsersProvider
+  ) {
     this.loadUser();
   }
-  goToProfile(selectedUser){
-    this.navCtrl.push(UserPage,{selectedUser})
+
+  public loadUser() {
+    this.usersProvider.getUsers().subscribe(response => {
+      this.users = response.results;
+      this.info = response.info;
+      console.log(this.users);
+      console.log(this.info);
+    });
   }
 
+
+  public goToProfile(selectedUser) {
+    this.navCtrl.push(UserPage, { selectedUser });
+  }
 }
--- src/providers/users/users.ts
+++ src/providers/users/users.ts
@@ -1,47 +1,33 @@
-
-import { Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 import "rxjs/Rx";
-
-
-/*
-  Generated class for the UsersProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 
 @Injectable()
 export class UsersProvider {
+  private apiURL = "https://randomuser.me/api/?results=10";
 
-  private apiURL="https://randomuser.me/api/?results=10"
+  constructor(public http: Http) {}
 
-  constructor(public http: Http) {
-
+  getUsers() {
+    return this.http
+      .get(this.apiURL)
+      .map((response: Response) => {
+        return response.json();
+      })
+      .catch(this.handleError);
   }
 
-  getUsers(){
-    return this.http.get(this.apiURL).map((response : Response)=>{
-      return response.json();
-
-    }
-    ).catch(this.handleError)
-  }
-
-  private handleError (error: Response | any) {
+  private handleError(error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
-      const body = error.json() || '';
+      const body = error.json() || "";
       const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+      errMsg = `${error.status} - ${error.statusText || ""} ${err}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
-
     }
     console.error(errMsg);
     return Observable.throw(errMsg);
-}
-
-
+  }
 }
