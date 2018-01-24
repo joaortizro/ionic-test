7aa1139553e0e484cf1095b2859f7ee842e29072
joaortizro <joaortizro@unal.edu.co>
2018-01-24 16:43:11 -0500

    EditPage for user

--- src/pages/user/user-edit/user-edit.ts
+++ src/pages/user/user-edit/user-edit.ts
@@ -1,11 +1,30 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
-
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 @IonicPage()
 @Component({
   selector: "page-user-edit",
   templateUrl: "user-edit.html"
 })
 export class UserEditPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  myForm: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.myForm = this.createUserForm();
+  }
+
+  private createUserForm() {
+    return this.formBuilder.group({
+      name: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", Validators.required],
+      dateBirth: ["", Validators.required],
+      passwordRetry: this.formBuilder.group({
+        password: ["", Validators.required],
+        passwordConfirmation: ["", Validators.required]
+      })
+    });
+  }
+  public saveData() {
+    console.log(this.myForm.value);
+  }
 }
