65603520eb54b9b77c211bb957e50f1ccdbbc527
joaortizro <joaortizro@unal.edu.co>
2018-01-24 14:58:51 -0500

    Facebook Auth

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, AlertController } from "ionic-angular";
 import { AuthenticationProvider } from "../../providers/authentication/authentication";
+import { HomePage } from "../home/home";
 @IonicPage()
 @Component({
   selector: "page-login",
@@ -9,13 +10,38 @@ import { AuthenticationProvider } from "../../providers/authentication/authentic
 export class LoginPage {
   public pushPage: any;
   public userPermissions: string[] = ["public_profile", "user_friends", "email"];
+  public isLogged: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthenticationProvider) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private auth: AuthenticationProvider,
+    public alerCtrl: AlertController
+  ) {}
 
-  public login(userPermissions) {
-    this.auth.login(userPermissions);
+  public login(userPermissions: string[]) {
+    let status: string;
+    this.auth.login(userPermissions).then(
+      response => {
+        status = response.status;
+        this.isLogged = true;
+        this.navCtrl.push(HomePage);
+      },
+      exception => {
+        this.showLoginAlert();
+      }
+    );
   }
   public logout() {
     this.auth.logout();
+  }
+
+  public showLoginAlert() {
+    let alert = this.alerCtrl.create({
+      title: "Error",
+      subTitle: "No esas autorizado para entrar",
+      buttons: ["OK"]
+    });
+    alert.present();
   }
 }
